7797f854744de361aa0b25a53dc34082cac5cb3a
SpeedsR <120658266+SpeedsR@users.noreply.github.com>
2022-12-15 22:52:38 +0530

    Create Speedsvariables.java

---
+++ Speedsvariables.java
@@ -0,0 +1,53 @@
+
+
+//package com.company;
+//public class SpeedsVariables {
+//        public static void main(String[ ] args){
+//        int i=6,j=8;
+//        System.out.println(6&8); // The & is a bitwise operator which will compare the two values in binary form and then return one
+//        // It works as 6=110 in binary form and 8=1000 in binary form
+//        // 110
+//        // 1000 first comparison between 1 which means true and another 1 so true and true will return true means 1
+//        // Second comparison between 1 and 0 which means false so true and false returns false means 0
+//        // Third comparison between 0 and 0 which will become false and false which will return false means 0
+//        // The last 0 will not be compared since there are no digits so it will return the value of last digit which is false because it is 0
+//        // The returned result becomes 1000 in binary which means 8 in decimal so it will return 8
+//    }
+//
+//}
+
+// // Encrypt a grade and then decrypt it
+//
+//package com.company;
+// public class SpeedsVariables {
+
+//    public static void main(String[] args){
+//        char grade= 'A';
+//        // // Encrypting the grade
+//        grade= (char)(grade+8); // // Typecasting the integer 8 into a char
+//        System.out.println(grade); // // This will return A + 8 letters after B which is I;
+//        // // Decrypting the grade
+//
+//        grade= (char)(grade-8);
+//        System.out.println(grade);
+//    }
+//}
+
+// // Check if user entered number is greater than the given number or not
+//
+//package com.company;
+//import java.util.Scanner;
+//public class SpeedsVariables {
+//
+//                public static void main(String[] args){
+//                    Scanner s= new Scanner(System.in);
+//                    int number= 5,userentered;
+//                    System.out.println("Enter a number here: ");
+//                    userentered= s.nextInt();
+//                    System.out.println(userentered>number);
+//                    float v=6f,u=2f,a=3f,k=5f;
+//                    float result= (v*v-u*u)/2*a*k;
+//                    System.out.println(result);
+//                }
+//            }
+
